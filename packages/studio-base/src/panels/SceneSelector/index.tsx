dcdddde9a2eeed34b6b18cb95538949208992830
WatanabeToshimitsu <toshimitsu.watanabe@hdwlab.co.jp>
2022-04-25 19:46:43 +0900

    Add hdl panels

---
+++ packages/studio-base/src/panels/SceneSelector/index.tsx
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+
+import { useRosLib, SceneSelector } from "scene-viewer-panels";
+import Panel from "@foxglove/studio-base/components/Panel";
+import PanelToolbar from "@foxglove/studio-base/components/PanelToolbar";
+import useGlobalVariables from "@foxglove/studio-base/hooks/useGlobalVariables";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const params = new URLSearchParams(window.location.search);
+const websocketUrl = params.get("ds.url") ?? "ws://localhost:9090";
+
+function SceneSelectorPanel() {
+  const { captionsWithLocation, seekToTimestamp } = useRosLib({
+    websocketUrl,
+    topicNames: ["/scene_viewer/scene_captions_with_locations"],
+  });
+  const { setGlobalVariables } = useGlobalVariables();
+
+  const setPinLocations = React.useCallback(
+    (pinLocations) => {
+      setGlobalVariables({ pinLocations });
+    },
+    [setGlobalVariables],
+  );
+  const captions = React.useMemo(
+    () =>
+      captionsWithLocation.map((item) => {
+        return {
+          timestamp: item.timestamp,
+          caption: item.caption,
+          location: {
+            altitude: item.altitude,
+            latitude: item.latitude,
+            longitude: item.longitude,
+          },
+        };
+      }),
+    [captionsWithLocation],
+  );
+
+  return (
+    <Container>
+      <PanelToolbar floating />
+      <SceneSelector
+        captions={captions}
+        setPinLocations={setPinLocations}
+        onSelectScene={(timestamp) => {
+          seekToTimestamp(timestamp);
+        }}
+      />
+    </Container>
+  );
+}
+
+SceneSelectorPanel.panelType = "SceneSelectorPanel";
+SceneSelectorPanel.defaultConfig = {};
+
+export default Panel(SceneSelectorPanel);
---
+++ packages/studio-base/src/panels/TrajectoryMap/index.tsx
@@ -0,0 +1,54 @@
+import { useRosLib, MapPanel as Map } from "scene-viewer-panels";
+import styled from "styled-components";
+
+import Panel from "@foxglove/studio-base/components/Panel";
+import PanelToolbar from "@foxglove/studio-base/components/PanelToolbar";
+import useGlobalVariables from "@foxglove/studio-base/hooks/useGlobalVariables";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const params = new URLSearchParams(window.location.search);
+const websocketUrl = params.get("ds.url") ?? "ws://localhost:9090";
+
+function MapPanel() {
+  const { currentPosition, trajectory } = useRosLib({
+    websocketUrl,
+    topicNames: ["/sensing/gnss/ublox/nav_sat_fix", "/scene_viewer/vehicle_trajectory"],
+  });
+
+  const { globalVariables } = useGlobalVariables();
+  const pinLocations = globalVariables.pinLocations || [];
+
+  return (
+    <Container>
+      <PanelToolbar floating />
+      <Map
+        centerPosition={[35.1505536926114, 136.96585423505437]}
+        currentPosition={[currentPosition.latitude, currentPosition.longitude]}
+        markers={pinLocations.map((item) => {
+          return {
+            longitude: item.longitude,
+            latitude: item.latitude,
+            popupText: item.description,
+            text: item.label,
+          };
+        })}
+        polylines={[
+          {
+            positions: trajectory.map((item) => {
+              return [item.latitude, item.longitude];
+            }),
+          },
+        ]}
+      />
+    </Container>
+  );
+}
+
+MapPanel.panelType = "TrajectoryMapPanel";
+MapPanel.defaultConfig = {};
+
+export default Panel(MapPanel);
