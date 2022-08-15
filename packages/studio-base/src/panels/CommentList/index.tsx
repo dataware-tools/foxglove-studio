6818d22fc70d652241b8a69bf795caeaa2ebe080
WatanabeToshimitsu <toshimitsu.watanabe@hdwlab.co.jp>
2022-08-15 16:00:57 +0900

    Fix component name

--- packages/studio-base/src/panels/CommentList/index.tsx
+++ packages/studio-base/src/panels/CommentList/index.tsx
@@ -2,6 +2,6 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { Experiment } from "scene-viewer-panels";
+import { CommentList } from "scene-viewer-panels";
 
-export default Experiment;
+export default CommentList;
