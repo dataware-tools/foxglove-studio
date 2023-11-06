3af804bf1c67378ed2bc9b0e6e39b684e806331c
Yusuke Sakai <y.f.sakai@gmail.com>
2023-11-06 16:05:05 +0900

    Fix eslint settings

--- packages/hdl-panels/.eslintrc.js
+++ packages/hdl-panels/.eslintrc.js
@@ -6,14 +6,14 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    "standard",
-    "standard-jsx",
-    "standard-react",
+    // "standard",
+    // "standard-jsx",
+    // "standard-react",
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
-    "plugin:eslint-comments/recommended",
+    // "plugin:eslint-comments/recommended",
     "plugin:import/errors",
     "plugin:import/warnings",
     "plugin:import/typescript",
@@ -31,8 +31,9 @@ module.exports = {
     "react",
     "@typescript-eslint",
     "import",
-    "unused-imports",
+    // "unused-imports",
     "react-hooks",
+    "prettier",
   ],
   settings: {
     react: {
@@ -41,38 +42,36 @@ module.exports = {
     "import/parsers": {
       "@typescript-eslint/parser": [".ts", ".tsx"],
     },
-    "import/resolver": {
-      typescript: {
-        alwaysTryTypes: true,
-        project: ["./tsconfig.json"],
-      },
-    },
+    // "import/resolver": {
+    //   typescript: {
+    //     alwaysTryTypes: true,
+    //     project: ["./tsconfig.json"],
+    //   },
+    // },
   },
   rules: {
-    "eslint-comments/no-unused-disable": "error",
-    // This rule make mean only when using "prop-type" library(https://ja.reactjs.org/docs/typechecking-with-proptypes.html)
+    "import/no-unresolved": "off",
+    // "eslint-comments/no-unused-disable": "error",
     "react/prop-types": "off",
     "react/no-unused-prop-types": "off",
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/FAQ.md#eslint-plugin-import
     "import/named": "off",
     "import/namespace": "off",
     "import/default": "off",
     "import/no-named-as-default-member": "off",
     indent: "off",
     "@typescript-eslint/indent": "off",
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/FAQ.md#i-am-using-a-rule-from-eslint-core-and-it-doesnt-work-correctly-with-typescript-code
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/FAQ.md#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
     "no-undef": "off",
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-use-before-define.md
     "no-use-before-define": "off",
     "@typescript-eslint/no-use-before-define": ["error"],
-    // in Next project, react need not be imported every file.
     "react/jsx-fragments": "off",
     "react/display-name": "off",
     "sort-imports": "off",
     "import/order": ["warn", { alphabetize: { order: "asc" } }],
-    "unused-imports/no-unused-imports": "error",
+    // "unused-imports/no-unused-imports": "error",
   },
+  // new configs to adapt current changes
+  ignorePatterns: ["src/panels/AnnotationManagement/ImageView"],
+  root: true,
 };
