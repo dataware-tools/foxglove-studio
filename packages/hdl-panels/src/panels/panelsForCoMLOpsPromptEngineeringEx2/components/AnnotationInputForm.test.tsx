61598270bb88363516f8cdf63468c379ef68ce4b
Toshimitsu Watanabe <kz86nu@gmail.com>
2024-07-19 08:04:51 +0000

    use findByRole instead of findByText when get button in test

--- packages/hdl-panels/src/panels/panelsForCoMLOpsPromptEngineeringEx2/components/AnnotationInputForm.test.tsx
+++ packages/hdl-panels/src/panels/panelsForCoMLOpsPromptEngineeringEx2/components/AnnotationInputForm.test.tsx
@@ -7,7 +7,7 @@ afterEach(cleanup);
 
 describe("AnnotationInputForm", () => {
   it("should render", async () => {
-    const { findByText, findByLabelText } = render(
+    const { findByText, findByLabelText, findByRole } = render(
       <PanelSetup>
         <AnnotationInputForm
           tagOptionsForEachTagType={[
@@ -27,14 +27,14 @@ describe("AnnotationInputForm", () => {
     expect(await findByText("Tag Type 1")).toBeDefined();
     expect(await findByLabelText("開始時刻")).toBeDefined();
     expect(await findByLabelText("終了時刻")).toBeDefined();
-    expect(await findByText("追加")).toBeDefined();
-    expect(await findByText("再生中の時刻を開始時刻に入力")).toBeDefined();
-    expect(await findByText("再生中の時刻を終了時刻に入力")).toBeDefined();
+    expect(await findByRole("button", { name: /追加/i })).toBeDefined();
+    expect(await findByRole("button", { name: /再生中の時刻を開始時刻に入力/i })).toBeDefined();
+    expect(await findByRole("button", { name: /再生中の時刻を終了時刻に入力/i })).toBeDefined();
   });
 
   it("should disable saving without valid input", async () => {
     const onSave = jest.fn();
-    const { findByText } = render(
+    const { findByRole } = render(
       <PanelSetup>
         <AnnotationInputForm
           tagOptionsForEachTagType={[
@@ -51,17 +51,17 @@ describe("AnnotationInputForm", () => {
       </PanelSetup>,
     );
 
-    const saveButton = await findByText("追加");
+    const saveButton = await findByRole("button", { name: /追加/i });
     expect(saveButton).toBeDefined();
     fireEvent.click(saveButton);
-    waitFor(async () => expect(await findByText("追加")).toBeDefined());
+    waitFor(async () => expect(await findByRole("button", { name: /追加/i })).toBeDefined());
 
     expect(onSave).not.toHaveBeenCalled();
   });
 
   it("should call onSave with the correct annotation", async () => {
     const onSave = jest.fn();
-    const { findByText } = render(
+    const { findByRole } = render(
       <PanelSetup>
         <AnnotationInputForm
           tagOptionsForEachTagType={[
@@ -87,7 +87,7 @@ describe("AnnotationInputForm", () => {
     );
 
     // click save button and wait call function
-    const saveButton = await findByText("Save");
+    const saveButton = await findByRole("button", { name: /Save/i });
     expect(saveButton).toBeDefined();
     fireEvent.click(saveButton);
     waitFor(() => expect(onSave).toHaveBeenCalled());
@@ -101,6 +101,6 @@ describe("AnnotationInputForm", () => {
     });
 
     // check if save button is back to the screen after loading
-    expect(await findByText("Save")).toBeDefined();
+    expect(await findByRole("button", { name: /Save/i })).toBeDefined();
   });
 });
