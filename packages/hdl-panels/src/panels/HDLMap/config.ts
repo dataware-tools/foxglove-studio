caf19bbc5fb22a1aa313cc55e2c0b4826fe910fb
Toshimitsu Watanabe <kz86nu@gmail.com>
2024-07-19 08:40:36 +0000

    Fix format of misc files

--- packages/hdl-panels/src/panels/HDLMap/config.ts
+++ packages/hdl-panels/src/panels/HDLMap/config.ts
@@ -1,11 +1,9 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
-
-import * as _ from "lodash-es";
-
 import { filterMap } from "@foxglove/den/collection";
 import { SettingsTreeFields, SettingsTreeNodes, Topic } from "@foxglove/studio";
+import * as _ from "lodash-es";
 
 // Persisted panel state
 export type Config = {
